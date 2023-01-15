b34e832d17af86663f9b6b27a042f25ecc3a70c7
XTGCODER <115632194+XTGCODER@users.noreply.github.com>
2023-01-15 13:32:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,8 +383,8 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://Clicksfly.com/api'
-    params = {'api': URL_SHORTNER_WEBSITE_API,
+    url = f'https://viplinks.io/api'
+    params = {'api': SHORTENER_API,
               'url': link,
               }
 
@@ -396,8 +396,8 @@ async def get_shortlink(link):
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://viplinks.io/api?api={SHORTENER_API}&url={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://viplinks.io/api?api={SHORTENER_API}&url={link}'
